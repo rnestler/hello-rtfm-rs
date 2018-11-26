2be9dc30161ef2f9b92e1d12f0f20e56c6428a64
Raphael Nestler <raphael.nestler@gmail.com>
2018-11-26 15:15:36 +0100

    Use rtfm

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate stm32f30x;
 
-use cortex_m::asm;
-use cortex_m_rt::entry;
+use rtfm::app;
 
-#[entry]
-fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+#[app(device = stm32f30x)]
+const APP: () = {
+    #[init]
+    fn init() {}
+};
 
-    loop {
-        // your code goes here
-    }
-}

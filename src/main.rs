e272114cb8ff34c2451a774d109e924bebcb9b44
Raphael Nestler <raphael.nestler@gmail.com>
2018-12-11 15:46:15 +0100

    LED roulette using timer and task

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,43 @@
 extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 extern crate stm32f30x;
 
-use rtfm::{self, app};
+use rtfm::{self, app, Instant};
 
 use f3::{
-    hal::{delay::Delay, prelude::*},
+    hal::prelude::*,
     led::Leds,
 };
+
+const PERIOD: u32 = 1_000_000;
 
 #[app(device = stm32f30x)]
 const APP: () = {
     static mut LEDS: Leds = ();
-    static mut DELAY: Delay = ();
 
-    #[init]
+    #[init(schedule = [leds])]
     fn init() {
         // device and core get injected by RTFM
-        let mut flash = device.FLASH.constrain();
         let mut rcc = device.RCC.constrain();
         let gpioe = device.GPIOE.split(&mut rcc.ahb);
-        let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
+        let now = Instant::now();
+        schedule.leds(now + PERIOD.cycles()).unwrap();
         LEDS = Leds::new(gpioe);
-        DELAY = Delay::new(core.SYST, clocks);
     }
 
-    #[idle(resources = [LEDS, DELAY])]
-    fn idle() -> ! {
-        let n = resources.LEDS.len();
-        loop {
-            for curr in 0..n {
-                let next = (curr + 1) % n;
-                resources.LEDS[curr].off();
-                resources.LEDS[next].on();
-
-                resources.DELAY.delay_ms(100_u8);
-            }
+    #[task(resources = [LEDS], schedule = [leds])]
+    fn leds() {
+        static mut curr: usize = 0;
+        schedule.leds(scheduled + PERIOD.cycles()).unwrap();
+        resources.LEDS[*curr].off();
+        *curr += 1;
+        if *curr > 7 {
+            *curr = 0;
         }
+        resources.LEDS[*curr].on();
+    }
+
+    extern "C" {
+        fn SPI1();
     }
 };
 

24e60a0f2a064b31649956e7bc047471e8fd08de
Raphael Nestler <raphael.nestler@gmail.com>
2018-12-28 19:19:22 +0100

    Locked shared resource

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,59 @@
-#![no_std]
+#![deny(unsafe_code)]
+#![deny(warnings)]
 #![no_main]
+#![no_std]
 
-extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
-extern crate stm32f30x;
+extern crate panic_semihosting;
 
-use rtfm::{self, app, Instant};
-
-use f3::{
-    hal::prelude::*,
-    led::Leds,
-};
-
-const PERIOD: u32 = 1_000_000;
+use cortex_m_semihosting::{debug, hprintln};
+use stm32f30x::Interrupt;
+use rtfm::app;
 
 #[app(device = stm32f30x)]
 const APP: () = {
-    static mut LEDS: Leds = ();
+    static mut SHARED: u32 = 0;
 
-    #[init(schedule = [leds])]
+    #[init]
     fn init() {
-        // device and core get injected by RTFM
-        let mut rcc = device.RCC.constrain();
-        let gpioe = device.GPIOE.split(&mut rcc.ahb);
-        let now = Instant::now();
-        schedule.leds(now + PERIOD.cycles()).unwrap();
-        LEDS = Leds::new(gpioe);
+        rtfm::pend(Interrupt::EXTI0);
     }
 
-    #[task(resources = [LEDS], schedule = [leds])]
-    fn leds() {
-        static mut curr: usize = 0;
-        schedule.leds(scheduled + PERIOD.cycles()).unwrap();
-        resources.LEDS[*curr].off();
-        *curr += 1;
-        if *curr > 7 {
-            *curr = 0;
-        }
-        resources.LEDS[*curr].on();
+    // when omitted priority is assumed to be `1`
+    #[interrupt(resources = [SHARED])]
+    fn EXTI0() {
+        hprintln!("A").unwrap();
+
+        // the lower priority task requires a critical section to access the data
+        resources.SHARED.lock(|shared| {
+            // data can only be modified within this critical section (closure)
+            *shared += 1;
+
+            // GPIOB will *not* run right now due to the critical section
+            rtfm::pend(Interrupt::SPI1);
+
+            hprintln!("B - SHARED = {}", *shared).unwrap();
+
+            // GPIOC does not contend for `SHARED` so it's allowed to run now
+            rtfm::pend(Interrupt::SPI2);
+        });
+
+        // critical section is over: GPIOB can now start
+
+        hprintln!("E").unwrap();
+
+        debug::exit(debug::EXIT_SUCCESS);
     }
 
-    extern "C" {
-        fn SPI1();
+    #[interrupt(priority = 2, resources = [SHARED])]
+    fn SPI1() {
+        // the higher priority task does *not* need a critical section
+        *resources.SHARED += 1;
+
+        hprintln!("D - SHARED = {}", *resources.SHARED).unwrap();
+    }
+
+    #[interrupt(priority = 3)]
+    fn SPI2() {
+        hprintln!("C").unwrap();
     }
 };
-
